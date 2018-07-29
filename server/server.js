2453336d190ddb496662a73063c7026e988ac56b
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2018-07-29 11:57:23 +0300

    Npm start listo

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ require('./config/config');
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser')
-    // parse application/x-www-form-urlencoded
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
